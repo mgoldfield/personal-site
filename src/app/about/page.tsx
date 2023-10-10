6386e157315350de75396580dfa2ed7c4b8a7fdb
Matt <mgoldfield@gmail.com>
2023-10-09 23:56:33 -0400

    config change

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,8 +7,8 @@ export default function About() {
       distributed systems, music, and combinations of those topics. I like
       thinking about how humans collaborate and build relationships, and about
       work and housing coops. I sometimes play and record music, and I cook and
-      bake a lot. I'm currently a software engineer at{" "}
-      <Link href="https://www.izotope.com/">iZotope</Link>. I'm an emeritus
+      bake a lot. I&apos;m currently a software engineer at{" "}
+      <Link href="https://www.izotope.com/">iZotope</Link>. I&apos;m an emeritus
       member at the worker coop{" "}
       <Link href="https://www.politicsrewired.com/">Politics Rewired</Link>.
       Various current and past software projects, including a (now inactive)
@@ -23,7 +23,7 @@ export default function About() {
       <Link href="https://elulmusicproject.bandcamp.com/">Cap and Scarf</Link>.
       From 2008-2015 I played in the rock band{" "}
       <Link href="https://therationales.bandcamp.com/">The Rationales</Link>.
-      I'm somewhat obsessed with cooking, especially smoking and curing,
+      I&apos;m somewhat obsessed with cooking, especially smoking and curing,
       fermenting, bread, pizza, noodles, and dumplings. Here are a few{" "}
       <Link href="https://photos.app.goo.gl/SdtHzGY2h6QRuHKm6">creations</Link>{" "}
       over the years.
