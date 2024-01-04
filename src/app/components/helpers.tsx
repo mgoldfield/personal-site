997e6ee4e074d68597ab5cf2f85c0d76dae6044d
Matthew Goldfield <mgoldfield@gmail.com>
2024-01-04 18:03:03 -0500

    static pages

--- src/app/components/helpers.tsx
+++ src/app/components/helpers.tsx
@@ -6,14 +6,13 @@ export function isProduction() {
   return process.env.NODE_ENV === "production";
 }
 
-export async function markdownPage(dir: string, name: string) {
-  const aboutMarkdownPath = path.join(process.cwd(), 'src', 'app', dir, name);
-  const aboutMarkdown = fs.readFileSync(aboutMarkdownPath).toString('utf-8');
-  const htmlToOutput = await transformMarkdown(aboutMarkdown);
-
+export async function markdownPage(dir: string, name: string, title: string) {
+  const markdownPath = path.join(process.cwd(), 'src', 'app', dir, name);
+  const markdown = fs.readFileSync(markdownPath);
+  const htmlToOutput = await transformMarkdown(markdown.toString());
   return (
     <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
-        <h1 className="text-3xl mt-4 mb-0">About</h1>
+        <h1 className="text-3xl mt-4 mb-0">{title}</h1>
         <article>
             <section dangerouslySetInnerHTML={{ __html: htmlToOutput}} />
         </article>
