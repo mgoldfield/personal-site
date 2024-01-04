68680099e7fcdf6b2441b97b07d4cdfaa71ac0d3
Matthew Goldfield <mgoldfield@gmail.com>
2024-01-04 18:24:33 -0500

    images etc

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,4 @@
-import { markdownPage } from "../components/helpers";
+import Image from 'next/image';
 
 export default async function Projects() {
     return (
@@ -10,20 +10,46 @@ export default async function Projects() {
             <a href="https://elulmusicproject.bandcamp.com/music" className="underline">High Holiday Albums with <em>Cap and Scarf</em></a>
             <div className="flex flex-row justify-between">
                 <a href="https://elulmusicproject.bandcamp.com/album/tishrei-the-end-is-the-beginning">
-                    <img src="https://f4.bcbits.com/img/a3430763899_10.jpg" className="w-64 m-0" />
+                    <Image 
+                        alt="Tishrei: The End is the Beginning album art" 
+                        src="https://f4.bcbits.com/img/a3430763899_10.jpg" 
+                        className="m-0"
+                        width={300}
+                        height={300} />
                 </a>
                 <a href="https://elulmusicproject.bandcamp.com/album/elul-songs-for-turning">
-                    <img src="https://f4.bcbits.com/img/a2638810254_10.jpg" className="w-64 m-0"/>
+                    <Image 
+                        alt="Elul album art" 
+                        src="https://f4.bcbits.com/img/a2638810254_10.jpg" 
+                        width={300}
+                        height={300}
+                        className="m-0"/>
                 </a>
             </div>
             <div className="prose-sm">
                 producer and multi-instrumentalist (co-produced with Anat Hochberg)
             </div>
         </section>
+        <section className='m-5'>
+            <a href="https://github.com/mgoldfield/nodelooper">
+                <div className='underline'>LoopMagic! in-browser DAW</div>
+                <Image
+                    alt="picture of loopmagic in action"
+                    src="/loopmagic.jpg"
+                    className='m-0'
+                    width={500}
+                    height={300} />
+            </a>
+        </section>
         <section className="m-5">
             <a href="https://therationales.bandcamp.com/">
                 <div className="underline">The Rationales</div>
-                <img src="https://f4.bcbits.com/img/a0668426547_10.jpg" className="w-64 m-0"/>
+                <Image 
+                    alt="rationales album art" 
+                    src="https://f4.bcbits.com/img/a0668426547_10.jpg" 
+                    className="m-0"
+                    width={300}
+                    height={300}/>
             </a>
             <div className="prose-sm">keyboards and bass in performances and recordings 2008-2014</div>
         </section>
